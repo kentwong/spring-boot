725be2ffac0444a1933d46857969c79b0f6e4cf0
Kent Wong <kent_wong@outlook.com>
2024-03-21 10:02:22 +1100

    initial upload

---
+++ src/main/java/com/kentwong/quickstart/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.kentwong.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello")
+    public String helloWord() {
+        return "Hello World";
+    }
+}
